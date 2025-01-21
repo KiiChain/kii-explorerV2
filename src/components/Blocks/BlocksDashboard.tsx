3555ac4fa7c8916ec9b173b838bb0f2b08ee24db
Dalejo505 <dg4m3s@gmail.com>
2025-01-21 14:07:31 -0500

    Fix font sizes

--- src/components/Blocks/BlocksDashboard.tsx
+++ src/components/Blocks/BlocksDashboard.tsx
@@ -298,17 +298,7 @@ export function BlocksDashboard() {
               <div
                 className="mt-4 text-xs p-3"
                 style={{ color: theme.quaternaryTextColor }}
-              >
-                <span className="flex items-center gap-2">
-                  <svg width="12" height="12" viewBox="0 0 16 16" fill="none">
-                    <path
-                      d="M8 1.33334C4.32 1.33334 1.33333 4.32001 1.33333 8.00001C1.33333 11.68 4.32 14.6667 8 14.6667C11.68 14.6667 14.6667 11.68 14.6667 8.00001C14.6667 4.32001 11.68 1.33334 8 1.33334ZM8.66667 11.3333H7.33333V7.33334H8.66667V11.3333ZM8.66667 6.00001H7.33333V4.66668H8.66667V6.00001Z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                  Only show txs in recent blocks
-                </span>
-              </div>
+              ></div>
             </div>
           )}
         </>
