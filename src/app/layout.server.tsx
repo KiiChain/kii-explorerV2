fc51ddac5c24cb6b579223f263062f1a6499a9eb
Dalejo505 <dg4m3s@gmail.com>
2025-03-10 17:06:18 -0500

    MEtadata KiiChain Explorer

---
+++ src/app/layout.server.tsx
@@ -0,0 +1,5 @@
+import { metadata } from "./metadata";
+import RootLayout from "./layoutClient";
+
+export { metadata };
+export default RootLayout;
--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "KiiChain",
+  title: "KiiChain Explorer",
 };
