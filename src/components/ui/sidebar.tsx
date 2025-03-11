82b29eb6c3694c43ddd48dc3207e91b6f4fc28c0
Dalejo505 <dg4m3s@gmail.com>
2025-03-11 14:58:45 -0500

    Fix height icons

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -620,8 +620,8 @@ const SidebarMenuButton = React.forwardRef<
       data-size={size}
       data-active={isActive}
       className={cn(
-        "flex h-20 min-w-0 items-center justify-start gap-6 overflow-hidden rounded-md px-6 text-sidebar-foreground outline-none   hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50",
-        "[&>span]:my-auto [&>svg]:my-auto",
+        "flex h-20 min-w-0 items-center justify-start gap-6 overflow-hidden rounded-md px-6 text-sidebar-foreground outline-none ring-sidebar-ring hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50",
+        "[&>span]:my-auto [&>svg]:translate-y-[-2px]",
         "[&>span:last-child]:truncate [&>svg]:h-20 [&>svg]:w-20 [&>svg]:shrink-0 [&>svg]:text-sidebar-accent-foreground",
         "data-[active=true]:bg-sidebar-accent data-[active=true]:text-sidebar-accent-foreground",
         size === "sm" && "text-xs",
