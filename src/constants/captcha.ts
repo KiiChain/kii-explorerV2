2c091e8be4a63319a2b8987af47b2d48cb2e5552
Andres Ramirez <andres.ramirez9912@gmail.com>
2025-06-25 16:57:18 -0500

    feat: migrated capcha

--- src/constants/captcha.ts
+++ src/constants/captcha.ts
@@ -1,2 +1,2 @@
-export const CAPTCHA_KEY = "6Le3ueUqAAAAAC_EJBqCnCBP0oGCD5s1tPpUZ6Bw"
-export const FAUCET_BACKEND = "https://faucet.kiivalidator.com"
+export const CAPTCHA_KEY = "0x4AAAAAABiZp9tfSOG5uzwv";
+export const FAUCET_BACKEND = "https://faucet.kiivalidator.com";
